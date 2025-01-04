c014f9ebcf6b99a7084a5063728576735c90ffcd
Diogo-Honorato <honoratodiogo097@gmail.com>
2025-01-04 15:29:23 -0300

    trocando malloc por calloc

--- Sources/PlagiChecker.c
+++ Sources/PlagiChecker.c
@@ -5,21 +5,20 @@
 PlagiChecker *iniciarPlagiChecker(int tamanhoString, int tamanhoSubString)
 {
 
-    PlagiChecker *plagiChecker = malloc(sizeof *plagiChecker);
+    PlagiChecker *plagiChecker = calloc(1,sizeof *plagiChecker);
 
-
-    plagiChecker->string = malloc(tamanhoString * sizeof *plagiChecker->string);
+    plagiChecker->string = calloc(tamanhoString, sizeof *plagiChecker->string);
     for (int i = 0; i < tamanhoString; i++)
     {
-        plagiChecker->string[i] = malloc(3 * sizeof **plagiChecker->string);
+        plagiChecker->string[i] = calloc(3, sizeof **plagiChecker->string);
     }
-    
 
-    plagiChecker->subString = malloc(tamanhoSubString * sizeof *plagiChecker->subString);
+
+    plagiChecker->subString = calloc(tamanhoSubString, sizeof *plagiChecker->subString);
     for (int i = 0; i < tamanhoSubString; i++)
     {
 
-        plagiChecker->subString[i] = malloc(3 * sizeof **plagiChecker->subString);
+        plagiChecker->subString[i] = calloc(3, sizeof **plagiChecker->subString);
     }
 
     plagiChecker->tamanhoString = tamanhoString;
