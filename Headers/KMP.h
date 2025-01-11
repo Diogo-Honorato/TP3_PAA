0551ab22582c23c94ee4c402e92c9da67d2c6706
Diogo-Honorato <honoratodiogo097@gmail.com>
2025-01-11 12:24:54 -0300

    Adicionando funçoes para o algoritmo KMP

---
+++ Headers/KMP.h
@@ -0,0 +1,9 @@
+#ifndef _KMP_H_
+#define _KMP_H_
+
+int kmp(char **texto, char **padrao, int tamanhoTexto, int tamanhoPadrao);
+
+int *preProcessamento(char **padrao, int tamanhoPadrao);
+
+
+#endif
