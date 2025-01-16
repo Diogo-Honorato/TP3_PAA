f19da8042be7ea165c8ffb7dec80abef055b7d05
Diogo-Honorato <honoratodiogo097@gmail.com>
2025-01-16 01:33:59 -0300

    Adicionando estrutura para mascara de bits

---
+++ Headers/SAE.h
@@ -0,0 +1,18 @@
+#ifndef _SAE_H_
+#define _SAE_H_
+
+typedef struct Item{
+
+    char *chave;
+    __int128_t item;
+
+}Item;
+
+typedef struct HashMap{
+
+    Item *array;
+    
+}HashMap;
+
+
+#endif
