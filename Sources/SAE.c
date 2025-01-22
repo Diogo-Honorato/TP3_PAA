34cd330edb274b9ef8addf73c6756de9a1a5d055
Diogo Augusto Martins Honorato <honoratodiogo097@gmail.com>
2025-01-22 14:53:45 -0300

    Update SAE.c

--- Sources/SAE.c
+++ Sources/SAE.c
@@ -27,21 +27,17 @@ int shiftAndExato(char *texto, int tamanhoTexto, int tamanhoPadrao, __int128_t *
     
     __int128_t matching = 0;
     
-    __int128_t alvo = (__int128_t)1 << (tamanhoPadrao - 1);
-
     for (int i = 0; i < tamanhoTexto; i++) {
         
         matching = (matching << 1) | (__int128_t)1;
 
-        __int128_t mascara = bitMasks[texto[i] - 'A'];
+        matching &= bitMasks[texto[i] - 'A'];
 
-        matching &= mascara; 
-
-        if (matching & alvo) {
-
-            return i - tamanhoPadrao + 1;
+         if (matching & ((__int128_t)1 << (tamanhoPadrao - 1))) {
+            
+            return i - tamanhoPadrao + 1; 
         }
     }
-
+    
     return -1;
 }
